12e5968694e28c1ab7dab962265681efc45d4cf5
Vasickin <vasickin@mail.ru>
2025-12-25 07:12:34 +0300

    Текущая ветка: refactor/architecture-cleanup src/main/java/com/community/cms/controller/projectAdmin/ProjectAdminController.java -> src/main/java/com/community/cms/web/mvc/controller/admin/content/ProjectAdminController.java

--- community-cms/src/main/java/com/community/cms/web/mvc/controller/admin/content/ProjectAdminController.java
+++ community-cms/src/main/java/com/community/cms/web/mvc/controller/admin/content/ProjectAdminController.java
@@ -1,4 +1,4 @@
-package com.community.cms.controller.projectAdmin;
+package com.community.cms.web.mvc.controller.admin.content;
 
 import com.community.cms.domain.model.content.PhotoGallery;
 import com.community.cms.dto.gallery.GalleryDTO;
