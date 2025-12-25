0de59d1d332d5419153ab3d7dc32bcae8da21573
Vasickin <vasickin@mail.ru>
2025-12-25 10:25:29 +0300

    Текущая ветка: refactor/architecture-cleanup переименовано: src/main/java/com/community/cms/config/JpaConfig.java -> src/main/java/com/community/cms/infrastructure/config/JpaConfig.java

--- community-cms/src/main/java/com/community/cms/infrastructure/config/JpaConfig.java
+++ community-cms/src/main/java/com/community/cms/infrastructure/config/JpaConfig.java
@@ -1,4 +1,4 @@
-package com.community.cms.config;
+package com.community.cms.infrastructure.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
